1e5cdc65f4fdf254b979cf9359e76fdd6a498b09
Devvrat Singh Rathod <73027078+Devvrat1010@users.noreply.github.com>
2023-02-04 21:20:44 +0530

    update models.py

--- hack_infinity/website/models.py
+++ hack_infinity/website/models.py
@@ -1,32 +1,63 @@
 from django.db import models
 
 # Create your models here.
-class Name(models.Model):
-    name=models.CharField('user name',max_length=32)
-    username=models.CharField('username',max_length=32,default=None)
+class Invitation(models.Model):
+    username=models.CharField('Username',max_length=32)
+    Invitee=models.CharField('Invitee',max_length=32)
     
 
 
-    def create(name):
-        book = Name(name=name)
+    def create(username,invitee):
+        book = Invitation(username=username,Invitee=invitee)
         # do something with the book
         return book
 
     def __str__(self):
-        return self.name
-
-class user_info(models.Model):
-    username=models.CharField('username',max_length=32)
-    invitee=models.CharField('invitee',max_length=32)
-    response=models.CharField('response',max_length=32)
-    
-    def create(name):
-        book = Name(name=name)
-        # do something with the book
-        return book
-
-    def __str__(self):
-        return self.name
+        return self.username
         
-class invitation(models.Model):
-    invitation=models.TextField('invitation',max_length=32)
+class CreateEventInfo(models.Model):
+    event_name=models.CharField('event_name',max_length=32)
+    venue=models.CharField('venue',max_length=32)
+    date=models.DateField('date',max_length=32)
+    sub_event=models.CharField('sub_event',max_length=32)
+    time=models.TimeField('time',max_length=32)
+    
+
+
+    def create(event_name,venue,date,sub_event,time):
+        book = CreateEventInfo(event_name=event_name,venue=venue,date=date,sub_event=sub_event,time=time)
+        # do something with the book
+        return book
+
+    def __str__(self):
+        return self.event_name
+
+class Invitation_confirmation(models.Model):
+    username=models.CharField('Username',max_length=32)
+    Invitee=models.CharField('Invitee',max_length=32)
+    Response=models.CharField('Response',max_length=32,default='No')
+    
+
+
+    def create(username,invitee,response):
+        book = Invitation(username=username,Invitee=invitee,Response=response)
+        # do something with the book
+        return book
+
+    def __str__(self):
+        return self.username
+
+# class user_info(models.Model):
+#     username=models.CharField('username',max_length=32)
+#     invitee=models.CharField('invitee',max_length=32)
+#     response=models.CharField('response',max_length=32)
+    
+#     def create(name):
+#         book = Name(name=name)
+#         # do something with the book
+#         return book
+
+#     def __str__(self):
+#         return self.name
+
+
