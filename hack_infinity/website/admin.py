f93fc0a6a0933fa212cb306536a0ea52122c1b38
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2023-02-05 01:34:30 +0530

    dhggfdh

--- hack_infinity/website/admin.py
+++ hack_infinity/website/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Invitation,Invitation_confirmation,CreateEventInfo
+from .models import Invitation,Invitation_confirmation,CreateEventInfo,events
 # Register your models here.
 admin.site.register(Invitation)
 admin.site.register(Invitation_confirmation)
 admin.site.register(CreateEventInfo)
+admin.site.register(events)
